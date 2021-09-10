9556d7cf3d1cd3dc789a83173e6b76594891d5eb
rbuisson <romain@mekomsolutions.com>
2021-09-10 11:16:02 +0200

    INFRA-259: Build and deploy images using GitHub Actions

---
+++ .github/resources/scripts/build_images.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+set -e
+services=$SERVICES
+
+echo "📑 Services to build: "
+echo $services
+echo "GITHUB_ENV:"
+echo "$GITHUB_ENV"
+echo "⚙️ Set the Revision:"
+REVISION=$(git rev-parse --short HEAD)
+
+DOCKER_USERNAME=mekomsolutions
+
+echo "⚙️ Run Docker build commands on remotes..."
+archs=arm64,amd64
+for arch in ${archs//,/ }
+do
+  ip=${!arch}
+  echo "Remote: $arch: $ip"
+
+  ssh -t -o StrictHostKeyChecking=no -i $AWS_AMI_PRIVATE_KEY_FILE -p 22 ubuntu@$ip /bin/bash -e << EOF
+cd repo/
+services=$services
+echo "⚙️ Will build the following list of services:" $services
+for service in \${services//,/ }
+do
+  echo "⚙️ Build '\${service}' image and tag it as '$DOCKER_USERNAME/\${service}:${REVISION}_$arch'..."
+  sudo docker build \${service}/ -t $DOCKER_USERNAME/\${service}:${REVISION}_${arch}
+done
+EOF
+done
