a400c530139f5d2c328506fee5195960c9f94fff
Zoheir CHINE <zoheir@mekomsolutions.com>
2022-11-24 14:11:38 +0000

    INFRA-348: Add name variable for filestore backup

--- filestore_backup/entrypoint.sh
+++ filestore_backup/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-cd ${FILESTORE_PATH} && zip -r ${BACKUP_PATH:-/opt/backup}/filestore-$(date +'%Y-%m-%d_%H-%M').zip ./
+cd ${FILESTORE_PATH} && zip -r ${BACKUP_PATH:-/opt/backup}/${FILENAME:-filestore}-$(date +'%Y-%m-%d_%H-%M').zip ./
