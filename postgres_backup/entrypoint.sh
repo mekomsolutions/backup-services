d650ed1495698d18c4b49d4a24ff7f988c9e2ce5
MOHANKUMAR T <mohan13081999@gmail.com>
2023-01-05 14:50:31 +0530

    Fix. PGPASSWORD variable naming

--- postgres_backup/entrypoint.sh
+++ postgres_backup/entrypoint.sh
@@ -9,9 +9,9 @@ chmod 600 ~/.pgpass
 # Wait for postgresql to initialize
 /opt/wait-for-it.sh --timeout=3600 ${DB_HOST:-localhost}:${DB_PORT:-5432}
 
-if [ "$( psql -h ${DB_HOST:-localhost} -U ${DB_USERNAME} -tAc "SELECT 1 FROM pg_database WHERE datname='${DB_NAME}'" )" = '1' ]
+if [ "$( PGPASSWORD=${DB_PASSWORD} psql -h ${DB_HOST:-localhost} -U ${DB_USERNAME} -tAc "SELECT 1 FROM pg_database WHERE datname='${DB_NAME}'" )" = '1' ]
 then
-    PG_PASSWORD="${DB_PASSWORD}" pg_dump -h ${DB_HOST:-localhost} -U ${DB_USERNAME} -F t -d ${DB_NAME} > ${BACKUP_PATH:-/opt/backup}/${DB_NAME}-$(date +'%Y-%m-%d_%H-%M').tar
+    PGPASSWORD="${DB_PASSWORD}" pg_dump -h ${DB_HOST:-localhost} -U ${DB_USERNAME} -F t -d ${DB_NAME} > ${BACKUP_PATH:-/opt/backup}/${DB_NAME}-$(date +'%Y-%m-%d_%H-%M').tar
 else
     echo "Database '${DB_NAME}' does not exist!"
     exit 1
